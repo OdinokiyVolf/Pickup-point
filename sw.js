23327f5580b9f959cd2943aebba2628ada0de861
OdinokiyVolf <123843210+OdinokiyVolf@users.noreply.github.com>
2026-05-03 16:11:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,18 @@
-self.addEventListener("fetch",()=>{});
+const CACHE_NAME = 'pvz-v2';
+const ASSETS = [
+  'index.html',
+  'style.css',
+  'manifest.json'
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((res) => res || fetch(event.request))
+    );
+});
